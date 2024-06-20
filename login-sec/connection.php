0ff3999afc7fe1a820f4a241a75cfba9f41e83d3
bebeciel835 <ejm.bhem@gmail.com>
2024-06-20 17:31:04 +0800

    clevercloud

--- login-sec/connection.php
+++ login-sec/connection.php
@@ -3,10 +3,10 @@
 function getDBConnection()
 {
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "bindtogether";
+    $servername = "b2iaftd53m7qfeckgdcd-mysql.services.clever-cloud.com";
+    $username = "ukmjtrrlicvc78dw";
+    $password = "9Q2W9VCnKUc60s9eYzr6";
+    $dbname = "b2iaftd53m7qfeckgdcd";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
